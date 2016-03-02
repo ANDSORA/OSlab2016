939591d9767d1f202358902a0b3b4e54f5a569ab
141220121-XuYin <1035378548@qq.com>
2016-03-02 23:48:05 +0800

    (lab1) Palette Fixed

--- kernel/device/video.c
+++ kernel/device/video.c
@@ -9,6 +9,7 @@ init_vmem() {
 	for(i = 0; i < SCR_HEIGHT; ++ i){
 		for(j = 0; j < SCR_WIDTH; ++ j){
 			int idx = (i << 8) + (i << 6) + j;
+			//vmem[idx] = j%256;
 			int midx = i * MOMOKO_WIDTH + j;
 			if(i < MOMOKO_HEIGHT && j < MOMOKO_WIDTH) vmem[idx] = MOMOKO_pixel[midx];
 			else vmem[idx] = 0;
